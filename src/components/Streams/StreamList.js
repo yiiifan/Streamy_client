aa9e1f36b221b8bfa8f9d720132008c578524a12
Ivana <ivana.zhangyf@gmail.com>
2019-08-12 18:36:31 -0400

    delete stream

--- src/components/Streams/StreamList.js
+++ src/components/Streams/StreamList.js
@@ -12,8 +12,16 @@ class StreamList extends React.Component {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">Edit</button>
-          <button className="ui button negative">Delete</button>
+          <Link className="ui button primary" to={`/streams/edit/${stream.id}`}>
+            Edit
+          </Link>
+
+          <Link
+            className="ui button negative"
+            to={`/streams/delete/${stream.id}`}
+          >
+            Delete
+          </Link>
         </div>
       );
     }
